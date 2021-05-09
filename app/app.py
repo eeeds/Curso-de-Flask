594e3b9d2e5eb78cffd4e298d44ffaf2209b70fa
eeeds <encinaesteban27@gmail.com>
2021-05-09 00:12:45 -0400

    añadiendo cambios al server

--- app/app.py
+++ app/app.py
@@ -2,7 +2,19 @@ from flask import Flask
 
 #Se le pasa ese parámetro para luego saber si se trabajará con un archivo principal
 app = Flask(__name__)
-
+#Se accede a la ruta raíz
+"""
+@app.route('/')
+def index():
+    return "Hola"
+"""
+def index():
+    return "Bienvendio:"
+#Esto se verá al ir a esa ruta
+@app.route('/holaMundo')
+def hola_mundo():
+    return "Hola mundo"
 #Si corremos como el main se inicia el servidor
 if __name__ == '__main__':
-    app.run()
+    app.add_url_rule('/', view_func = index)
+    app.run(debug = True, port = 5005)
